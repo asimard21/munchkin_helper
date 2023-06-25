8ec262f493f72aa1cefa68178697f64e3da667bc
asimard21 <100657046+asimard21@users.noreply.github.com>
2023-06-25 15:33:07 -0400

    Changed the base path of the program

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/munchkin-helper',
+  base: '/munchkin_helper',
   plugins: [react()],
   resolve: {
   alias: {
